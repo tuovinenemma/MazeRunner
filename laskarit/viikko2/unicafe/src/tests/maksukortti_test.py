03f4104a4c41088055d1830f2f4cfc82aa2a5eaf
tuovinenemma <102189885+tuovinenemma@users.noreply.github.com>
2022-11-15 20:57:07 +0200

    Update maksukortti_test.py

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -3,7 +3,28 @@ from maksukortti import Maksukortti
 
 class TestMaksukortti(unittest.TestCase):
     def setUp(self):
-        self.maksukortti = Maksukortti(1000)
+        self.maksukortti = Maksukortti(10)
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(990)
+        self.assertEqual(str(self.maksukortti), "saldo: 10.0")
+
+    def test_saldo_vahenee_jos_tarpeeksi_rahaa(self):
+        self.maksukortti.ota_rahaa(5)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.05")
+
+    def test_saldo_ei_muutu_jos_rahaa_ei_ole_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(100)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
+
+    def test_metodi_palauttaa_true_jos_tarpeeksi_rahaa(self):
+        self.assertTrue(self.maksukortti.ota_rahaa(5))
+
+    def test_metodi_palauttaa_false_jos_ei_tarpeeksi_rahaa(self):
+        self.assertFalse(self.maksukortti.ota_rahaa(1000))
