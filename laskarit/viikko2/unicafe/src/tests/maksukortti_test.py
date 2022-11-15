d825f47e4124b26ece09e4b2384c9f7a9d29deb7
tuovinenemma <102189885+tuovinenemma@users.noreply.github.com>
2022-11-15 21:20:15 +0200

    Update maksukortti_test.py

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -8,12 +8,15 @@ class TestMaksukortti(unittest.TestCase):
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
 
+    def test_luotu_kortti_on_olemassa(self):
+        self.assertNotEqual(self.maksukortti, None)
+
     def test_kortin_saldo_alussa_oikein(self):
-        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 0.1 euroa")
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 0.10 euroa")
 
     def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
         self.maksukortti.lataa_rahaa(990)
-        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.0 euroa")
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
 
     def test_saldo_vahenee_jos_tarpeeksi_rahaa(self):
         self.maksukortti.ota_rahaa(5)
@@ -21,7 +24,7 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_saldo_ei_muutu_jos_rahaa_ei_ole_tarpeeksi(self):
         self.maksukortti.ota_rahaa(100)
-        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 0.1 euroa")
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 0.10 euroa")
 
     def test_metodi_palauttaa_true_jos_tarpeeksi_rahaa(self):
         self.assertTrue(self.maksukortti.ota_rahaa(5))
